d997c877009e11800497e9bcf557654bc38cd52d
Malak4m <mstfythmanmlk@gmail.com>
2025-03-20 16:13:04 +0200

    video player

---
+++ Assets/Malak/Scripts/VideoManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer; // ربط فيديو بلاير
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd; // تشغيل الدالة عند انتهاء الفيديو
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // الانتقال للمشهد التالي
+    }
+}
+
