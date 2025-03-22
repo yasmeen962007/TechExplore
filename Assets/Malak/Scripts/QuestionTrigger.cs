714f195db726b3be7e7eaebf03bddc1846db5f3a
Malak4m <mstfythmanmlk@gmail.com>
2025-03-22 17:44:53 +0200

    Door Animation

---
+++ Assets/Malak/Scripts/QuestionTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class QuestionTrigger : MonoBehaviour
+{
+    public Fungus.Flowchart flowchart;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            flowchart.ExecuteBlock("Start"); // نفذ Block باسم "Start"
+        }
+    }
+}
