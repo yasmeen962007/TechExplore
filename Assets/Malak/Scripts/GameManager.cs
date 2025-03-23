c367cd14632857cc3124adfdce1b44e68da12c20
MrH008 <hazmsaleh35@gmail.com>
2025-03-23 11:56:04 +0200

    GameManager Done

---
+++ Assets/Malak/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public enum GameState
+    {
+        Start,
+        Win,
+        Lose
+    }
+
+    public GameState CurrentState;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Keep GameManager across scenes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SetGameState(GameState newState)
+    {
+        if (CurrentState == newState) return; // Prevent unnecessary state changes
+
+        CurrentState = newState;
+    }
+
+}
