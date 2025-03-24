3b3ff14af69ec040a01ec747664f00ac51f08ec2
yasmeen962007 <yasmeenyaseer92@gmail.com>
2025-03-24 10:37:54 +0200

    all dialog

---
+++ Assets/Script/Keyboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Keyboard : MonoBehaviour
+{
+    public Flowchart flowchart;  // متغير لحفظ الفلو شارت
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  // التأكد أن اللاعب دخل منطقة الطابعة
+        {
+            flowchart.ExecuteBlock("KeyboardSay");  // تشغيل بلوك الحوار
+        }
+    }
+}
