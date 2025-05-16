095c754c04de0e6c794cd775748399ed36e45444
yasmeen962007 <yasmeenyaseer92@gmail.com>
2025-05-16 14:44:57 +0300

    dialog

---
+++ Assets/Yasmeen/Script/witch.cs
@@ -0,0 +1,23 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class witch : MonoBehaviour
+{
+    public Flowchart flowchart;  // متغير لحفظ الفلو شارت
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // يمكنك وضع إعدادات مبدئية هنا إن وجدت
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))  // التأكد أن اللاعب دخل منطقة الساحرة
+        {
+            flowchart.ExecuteBlock("StartSay");  // تشغيل بلوك الحوار
+        }
+    }
+}
